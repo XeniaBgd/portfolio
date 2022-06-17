ed9169cb26be1a0460377b6fdeac34e9eb53d9d3
xenia <bogd.kseniya050199@mail.ru>
2022-06-17 21:37:30 +0300

    добавлена сортировка навыков

--- script.js
+++ script.js
@@ -1,46 +1,95 @@
-let skillList = [
-    {
-        name:"HTML", 
-        level: "30%",       
-    },
-    {
-        name:"CSS", 
-        level: "40%",       
-    },
-    {
-        name:"JS", 
-        level: "30%",       
-    },
-    {
-        name:"C++", 
-        level: "72%",      
-    },
-    {
-        name:"Go", 
-        level: "50%",        
-    },
-    {
-        name:"SQL", 
-        level: "60%",        
+function SkillList() {
+    class Skill {
+        constructor(name, level) {
+            this.name = name;
+            this.level = level;
+        }
     }
-]
 
-let skillsListElement = document.querySelector(".skills-list")
-skillList.forEach(skill => {
-    let decl = document.createElement("dt")
-    // image name format: img/icon-{lowercase name}.svg
-    decl.style.backgroundImage = `url('img/icon-${skill.name.toLowerCase()}.svg')`
-    decl.innerHTML = skill.name 
+    function SortByLevel(){       
+        let button = document.querySelector(".by-level");
+        button.desc = false;
+        button.onclick = function(){
+            if(this.desc){
+                this.desc = false;
+                skills.list.sort((a, b) => Number(a.level.slice(0,-2)) > Number(b.level.slice(0,-2)));                                  
+            }
+            else{
+                this.desc = true;
+                skills.list.sort((a, b) => Number(a.level.slice(0,-2)) < Number(b.level.slice(0,-2)));     
+                    
+            }
+            skills.generateElements();
+            skills.resetSortedButtons(button);
+        }       
+    }
 
-    let level = document.createElement("dd")
-    level.classList += "level"
+    function SortByName(){       
+        let button = document.querySelector(".by-name");
+        button.desc = false;
+        button.onclick = function(){
+            if(this.desc){
+                this.desc = false;                
+                skills.list.sort((a, b) => a.name < b.name);
+                //skills.list = skills.list.reverse();
+               
+            }
+            else{
+                this.desc = true;
+                skills.list.sort((a, b) => a.name > b.name);                
+            }
+            skills.generateElements();
+            skills.resetSortedButtons(button);
+        }     
+    }
+    SortByLevel();
+    SortByName();
 
-    let container = document.createElement("div")
-    container.style.width = skill.level
-    container.textContent = skill.level   
-    level.appendChild(container) 
+    return {    
+        sortedBy: "",   
+        list:
+        [
+            new Skill("HTML", "30%"),
+            new Skill("CSS", "40%"),
+            new Skill("JS", "30%"),
+            new Skill("C++", "72%"),
+            new Skill("Go", "50%"),
+            new Skill("SQL", "60%")
+        ],
+        generateElements: function(){              
+            skillsListElement.innerHTML = '';
+            this.list.forEach(skill => {
+                let decl = document.createElement("dt");
+                // image name format: img/icon-{lowercase name}.svg
+                decl.style.backgroundImage = `url('img/icon-${skill.name.toLowerCase()}.svg')`;
+                decl.innerHTML = skill.name ;
+            
+                let level = document.createElement("dd");
+                level.classList += "level";
+            
+                let container = document.createElement("div");
+                container.style.width = skill.level;
+                container.textContent = skill.level  ; 
+                level.appendChild(container);
+            
+                skillsListElement.appendChild(decl);
+                skillsListElement.appendChild(level);    
+            });           
+        },  
+        /* resetting a saved sort when sorting by another sort button */
+        resetSortedButtons(button){
+            if(skills.sortedBy != button){
+                skills.sortedBy.desc = false;
+                skills.sortedBy = button;
+            }
+        }      
+    }        
+}
 
-    skillsListElement.appendChild(decl)
-    skillsListElement.appendChild(level)    
-});
+const skillsListElement = document.querySelector(".skills-list");
+
+let skills = new SkillList()
+skills.generateElements()
+
+
 
